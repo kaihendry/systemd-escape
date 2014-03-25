9a6bad7ce341bf8178e8448a8d7fa831e5f56a24
Kai Hendry <hendry@webconverger.com>
2014-03-25 17:29:41 +0800

    Tidy up

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 function ascii(a) {
-	//return a.charCodeAt(0);
 	var code = a.charCodeAt(0);
-	//var codeHex = code.toString(16).toUpperCase();
 	var codeHex = "\\x" + code.toString(16);
-	//while (codeHex.length < 4) {
-	//	codeHex = "0" + codeHex;
-	//}
 	return codeHex;
 }
 
